de22bb29d89b89480661aba37938a9e8c5760a35
William <billy.ryan.rom@gmail.com>
2020-03-25 17:24:53 -0400

    Add repair function and test for the repair function

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -3,7 +3,21 @@ const {repair, succeed, fail, get} = require('./enhancer.js');
 describe('enhancer', function (){
     describe('repair()', function(){
         it('Should return a new item with the durability restored to 100', function() {
-            
+            const item = {
+                name: 'Sword',
+                durability: 10,
+                enhancement: 15
+            };
+
+            const expected = {
+                name: 'Sword',
+                durability: 100,
+                enhancement: 15
+            };
+
+            const repairedItem = repair(item)
+
+            expect(repairedItem).toEqual(expected)
         })
     })
 })
